a4cedea66436ba2f0808e4c369c56bf508776941
Roberto Estrada <roberto.estrada.ext@sicam.mx>
2014-09-04 19:28:51 -0500

    minor changes

--- dBase.py
+++ dBase.py
@@ -10,34 +10,46 @@ __author__ = 'Roberto Estrada'
 __version__ = "$Revision: e98737176f1d $"
 # $Source$
 import os
+import sys
 from dbfpy import dbf
-
-'''
- Configure section
- RUN_DIR Specifies the path of the current directory
-'''
-RUN_DIR = ('/').join(os.path.abspath(__file__).split('/')[0:-1])
 
 class MigrationFromDBase:
     '''
         This class works for migrate the dBase to orm django
         actually tested only on pypy
     '''
-    def fetch_dbase_table(self, table, keys):
+    '''
+     Configure section
+     TABLES_DIR Specifies the path of the current directory
+    '''
+    TABLES_PATH = ('/').join(os.path.abspath(__file__).split('/')[0:-1]) + '/tables'
+
+    def fetch_dbase_table(self, table, keys, path=None):
         ''' Fetches rows from a dBase Table
 
         Retrieves rows pertaining to the given keys from the Table instance
 
         :param table:  the name of the table
         :param keys: the list of keys
+        :param path: Optional value, if you don't specify this value
+         it will take it the default TABLES_PATH
         :return list: Return a list with internal dictionary
         '''
-        db = dbf.Dbf("%s/tables/%s" % (RUN_DIR, table))
+        if not path is None:
+            if os.path.isdir(path):
+                if path[-1] == '/':
+                    self.TABLES_PATH = path[0:-1]
+                else:
+                    self.TABLES_PATH = path
+            else:
+                self.error(-1)
+        db = dbf.Dbf("%s/%s" % (self.TABLES_PATH, table))
         dbase = []
         for r in db:
             for k in keys:
                 dbase.append({k: r[k]})
         return dbase
+
 
     def fetch_dbase_keys(self, table):
         ''' Fetches keys from a dBase Table
@@ -47,16 +59,25 @@ class MigrationFromDBase:
         :param table:  the name of the table
         :return list: Return a list with keys from the table
         '''
-        db = dbf.Dbf("%s/tables/%s" % (RUN_DIR, table))
-        dbkeys = []
+        db = dbf.Dbf("%s/%s" % (self.TABLES_PATH, table))
+        dbase_keys = []
         for r in db.fieldNames:
-            dbkeys.append(r)
-        return dbkeys
+            dbase_keys.append(r)
+        return dbase_keys
+
+
+    def set_path(self, path):
+        self.TABLES_DIR = path
+
+    def error(self,id):
+        if id == -1:
+            error = "Error the path is not a directory"
 
 # Testing tool
 if __name__ == '__main__':
     m = MigrationFromDBase()
-    menus = m.fetch_dbase_table('ACCMENU.DBF', ['NOMBRE'])
+    # specify path
+    menus = m.fetch_dbase_table('ACCMENU.DBF', ['NOMBRE'], '/Volumes/home/paridin/Devel/python/ppm/migrations/')
     for menu in menus:
         print (menu['NOMBRE'])
 
